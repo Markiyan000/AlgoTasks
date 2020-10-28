1156b4b3b03fd3e6b6a93c21059c45ebc16d72ea
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-10-28 22:16:56 +0200

    added FibonacciNumberByIndex task

---
+++ src/main/java/fibonacci/FibonacciNumberByIndex.java
@@ -0,0 +1,31 @@
+package fibonacci;
+
+public class FibonacciNumberByIndex {
+
+    // 0 -> 1 -> 1 -> 2 -> 3 -> 5
+
+    private static int findFibonacciNumberByIndex(int index) {
+        int prev = 0;
+        int current = 1;
+        int next = 0;
+        if (index == 1) return prev;
+        if (index == 2) return current;
+
+        for (int i = 2; i < index; i++) {
+            next = prev + current;
+            prev = current;
+            current = next;
+        }
+
+        return next;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findFibonacciNumberByIndex(1));
+        System.out.println(findFibonacciNumberByIndex(2));
+        System.out.println(findFibonacciNumberByIndex(10));
+        System.out.println(findFibonacciNumberByIndex(5));
+        System.out.println(findFibonacciNumberByIndex(8));
+        System.out.println(findFibonacciNumberByIndex(7));
+    }
+}
