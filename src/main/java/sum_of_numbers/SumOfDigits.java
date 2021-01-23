231e2ae0c459ea835d8afbbe16e95a75acdc21b9
Markiyan Derevetskyi <markiyan07345@gmail.com>
2021-01-23 15:08:51 +0200

    added another solution to SumOfDigits task

--- src/main/java/sum_of_numbers/SumOfDigits.java
+++ src/main/java/sum_of_numbers/SumOfDigits.java
@@ -8,8 +8,22 @@ public class SumOfDigits {
         return Arrays.stream(number.split("")).mapToInt(Integer::valueOf).sum();
     }
 
+    public static int calculateSumOfDigits(String string) {
+        int number = Integer.parseInt(string);
+        int sum = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            sum += lastDigit;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         String number = "12345";
         System.out.println(calculateSumOfDigitsFunctional(number));
+        System.out.println(calculateSumOfDigits(number));
     }
 }
